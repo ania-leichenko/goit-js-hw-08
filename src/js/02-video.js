8ab9b20505fcfd00042446598e41511c96ce66c5
Leichenko Anna <leichenkoanna@MacBook-Air-Leichenko.local>
2022-08-27 15:07:27 +0200

    finish task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,16 @@
 import Player from '@vimeo/player';
 import { throttle } from 'lodash';
 
+const STORAGE_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 const onPlay = function (data) {
-  localStorage.setItem('videoplayer-current-time', data.seconds);
+  localStorage.setItem(STORAGE_KEY, data.seconds);
 };
 player.on('timeupdate', throttle(onPlay, 1000));
 
-const secondsItem = localStorage.getItem('videoplayer-current-time');
+const secondsItem = localStorage.getItem(STORAGE_KEY);
 if (secondsItem) {
   player
     .setCurrentTime(secondsItem)
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('.feedback-form textarea');
+const input = document.querySelector('input');
+const STORAGE_KEY = 'feedback-form-state';
+import throttle from 'lodash.throttle';
+form.addEventListener('input', throttle(saveInputValues, 500));
+form.addEventListener('submit', submitFunc);
 
+const obj = {};
+reloadPage();
+
+function saveInputValues(e) {
+  obj[e.target.name] = e.target.value;
+  const stringifiedData = JSON.stringify(obj);
+  localStorage.setItem(STORAGE_KEY, stringifiedData);
+}
+
+function reloadPage() {
+  const saveValues = localStorage.getItem(STORAGE_KEY);
+
+  if (saveValues === null) {
+    return;
+  } else {
+    textarea.value = saveValues['message'] || '';
+    input.value = saveValues['email'] || '';
+  }
+}
+
+function submitFunc() {
+  console.log(obj);
+  localStorage.removeItem(STORAGE_KEY);
+}
