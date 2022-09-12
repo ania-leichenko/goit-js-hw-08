8d055bdcca5eaee200e854c0e2c1a50f07ada7a0
Leichenko Anna <leichenkoanna@air-leichenko.mynet>
2022-09-12 20:10:54 +0100

    fix 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,4 @@
 const form = document.querySelector('.feedback-form');
-const textarea = document.querySelector('.feedback-form textarea');
-const input = document.querySelector('input');
 const STORAGE_KEY = 'feedback-form-state';
 import throttle from 'lodash.throttle';
 form.addEventListener('input', throttle(saveInputValues, 500));
@@ -16,17 +14,19 @@ function saveInputValues(e) {
 }
 
 function reloadPage() {
-  const saveValues = localStorage.getItem(STORAGE_KEY);
+  let saveValues = localStorage.getItem(STORAGE_KEY);
 
-  if (saveValues === null) {
-    return;
-  } else {
-    textarea.value = saveValues['message'] || '';
-    input.value = saveValues['email'] || '';
+  if (saveValues) {
+    saveValues = JSON.parse(saveValues);
+    console.log(saveValues);
+    Object.entries(saveValues).forEach(([name, value]) => {
+      form.elements[name].value = value;
+    });
   }
 }
 
 function submitFunc() {
+  e.preventDefault();
   console.log(obj);
   localStorage.removeItem(STORAGE_KEY);
 }
